2b91a1601c2a7fd8eb5343d40161fa08b1618521
Francisco Souza <francisco@franciscosouza.net>
2010-07-17 11:31:54 -0300

    Added another simple very simple example. Now the environment is setup

--- talks_application/views.py
+++ talks_application/views.py
@@ -1,5 +1,10 @@
+#coding:utf-8
 from talks_application import app
 
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/hello/<nome>')
+def ser_educado(nome):
+    return 'Oi %s' %nome
