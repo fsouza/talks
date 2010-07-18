c7bb729b9bfd62530526c288d5a5ae287f9e2848
Francisco Souza <francisco@franciscosouza.net>
2010-07-18 17:24:11 -0300

    Changed model: presented_by field should be required

--- talks_application/models.py
+++ talks_application/models.py
@@ -4,4 +4,4 @@ class Talk(db.Model):
     title = db.StringProperty(required=True)
     description = db.StringProperty(required=True, multiline=True)
     date = db.DateTimeProperty(required=True)
-    presented_by = db.UserProperty()
+    presented_by = db.UserProperty(required=True)
