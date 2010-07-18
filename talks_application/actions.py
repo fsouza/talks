fd92ca5e6f7d38533c07b02404c40d968d7ab2cc
Francisco Souza <francisco@franciscosouza.net>
2010-07-18 20:19:58 -0300

    Listining all registered talks in index

--- talks_application/actions.py
+++ talks_application/actions.py
@@ -6,12 +6,14 @@ from talks_application.decorators import login_required
 
 from google.appengine.api import users
 
-from flask import redirect
-from flask import render_template
+from flask import redirect, render_template, flash, url_for
+
+from datetime import datetime
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    talks = Talk.all().order('-date');
+    return render_template('/talks/index.html', talks=talks)
 
 @app.route('/login')
 def redirect_to_login():
@@ -25,4 +27,20 @@ def redirect_to_logout():
 @login_required
 def new_talk():
     form = TalkForm()
+    return render_template('/talks/new.html', form=form)
+
+@app.route('/talks', methods=['POST'])
+@login_required
+def create_talk():
+    form = TalkForm()
+    if form.validate_on_submit():
+        talk = Talk(
+            title = form.title.data,
+            date = form.date.data,
+            description = form.description.data,
+            presented_by = users.get_current_user()
+        )
+        talk.put()
+        flash('Talk saved in the database')
+        return redirect(url_for('index'))
     return render_template('/talks/new.html', form=form)
