8ccb52552304b5ffcaf7f8813ece73671551ebd3
Francisco Souza <francisco@franciscosouza.net>
2010-07-18 16:52:12 -0300

    Added actions to login and logout

--- talks_application/actions.py
+++ talks_application/actions.py
@@ -1,8 +1,19 @@
 #coding:utf-8
 from talks_application import app
 from talks_application.models import *
-from datetime import datetime
+
+from google.appengine.api import users
+
+from flask import redirect
 
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/login')
+def redirect_to_login():
+    return redirect(users.create_login_url('/'))
+
+@app.route('/logout')
+def redirect_to_logout():
+    return redirect(users.create_logout_url('/'))
