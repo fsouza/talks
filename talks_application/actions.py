afa42d90b0a0daffa03a783814e0b23d0c49fa6f
Francisco Souza <francisco@franciscosouza.net>
2010-07-18 19:54:33 -0300

    Created the page with the form to save a talk

--- talks_application/actions.py
+++ talks_application/actions.py
@@ -1,10 +1,13 @@
 #coding:utf-8
 from talks_application import app
 from talks_application.models import *
+from talks_application.forms import *
+from talks_application.decorators import login_required
 
 from google.appengine.api import users
 
 from flask import redirect
+from flask import render_template
 
 @app.route('/')
 def index():
@@ -17,3 +20,9 @@ def redirect_to_login():
 @app.route('/logout')
 def redirect_to_logout():
     return redirect(users.create_logout_url('/'))
+
+@app.route('/talks/new', methods=['GET'])
+@login_required
+def new_talk():
+    form = TalkForm()
+    return render_template('/talks/new.html', form=form)
