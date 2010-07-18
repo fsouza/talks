c86b6077063e144beb6df745d977d5ea9d158947
Francisco Souza <francisco@franciscosouza.net>
2010-07-18 20:15:54 -0300

    Changed model, to use a date field instead of a datetime field

--- talks_application/models.py
+++ talks_application/models.py
@@ -3,5 +3,5 @@ from google.appengine.ext import db
 class Talk(db.Model):
     title = db.StringProperty(required=True)
     description = db.StringProperty(required=True, multiline=True)
-    date = db.DateTimeProperty(required=True)
+    date = db.DateProperty(required=True)
     presented_by = db.UserProperty(required=True)
