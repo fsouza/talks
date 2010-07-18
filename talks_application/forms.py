0704413e12bed7c79bad85feb743879bdfb90885
Francisco Souza <francisco@franciscosouza.net>
2010-07-18 19:36:39 -0300

    Updated forms

--- talks_application/forms.py
+++ talks_application/forms.py
@@ -4,4 +4,4 @@ from flaskext.wtf import validators
 class TalkForm(wtf.Form):
     title = wtf.TextField('Title', validators=[validators.Required()])
     description = wtf.TextAreaField('Description', validators=[validators.Required()])
-    date = wtf.DateField('Date', validators[validators.Required()], format='%m/%d/%Y')
+    date = wtf.DateField('Date', validators=[validators.Required()], format='%m/%d/%Y')
