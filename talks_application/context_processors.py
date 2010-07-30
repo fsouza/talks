a59757759fd4fc3289ba78726b21784c154236a5
Francisco Souza <francisco@franciscosouza.net>
2010-07-30 18:58:29 -0300

    Adding user in context

--- talks_application/context_processors.py
+++ talks_application/context_processors.py
@@ -1,6 +1,14 @@
-from google.appengine.api.users import create_login_url, create_logout_url
+from google.appengine.api import users
 from talks_application import app
 
 @app.context_processor
 def inject_login_url():
-    return dict(create_login_url=create_login_url, create_logout_url=create_logout_url)
+    return dict(create_login_url=users.create_login_url, create_logout_url=users.create_logout_url)
+
+@app.context_processor
+def inject_user():
+    try:
+        user = users.User()
+    except users.UserNotFoundError:
+        user = None
+    return {'user' : user}
--- tests/unit/test_context_processors.py
+++ tests/unit/test_context_processors.py
@@ -1,11 +1,43 @@
 import unittest
+import mocker
 from nose.tools import assert_equals
 
 class TestContextProcessors(unittest.TestCase):
+
+    def setUp(self):
+        self.mocker = mocker.Mocker()
 
     def test_create_login_and_logout_url_injector(self):
         "Should exists a context processor to inject the Google App Engine login and logout URLs in the context"
         from google.appengine.api.users import create_login_url, create_logout_url
         from talks_application.context_processors import inject_login_url
         assert_equals(inject_login_url(), dict(create_login_url=create_login_url, create_logout_url=create_logout_url))
+
+    def test_inject_logged_user(self):
+        "Should inject the current user in the template context when there is a logged in user"
+        from google.appengine.api.users import User
+        user = self.mocker.mock(User)
+        google_user = self.mocker.replace('google.appengine.api.users.User')
+        google_user()
+        self.mocker.result(user)
+        self.mocker.replay()
+
+        from talks_application.context_processors import inject_user
+        assert_equals(inject_user(), dict(user=user))
+        self.mocker.verify()
+
+    def test_inject_none_when_not_logged_user(self):
+        "Should inject None on user variable in the template context when there is not a logged in user"
+        from google.appengine.api.users import UserNotFoundError
+        google_user = self.mocker.replace('google.appengine.api.users.User')
+        google_user()
+        self.mocker.throw(UserNotFoundError)
+        self.mocker.replay()
+
+        from talks_application.context_processors import inject_user
+        assert_equals(inject_user(), dict(user=None))
+        self.mocker.verify()
+
+    def tearDown(self):
+        self.mocker.restore()
 
