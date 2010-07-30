4f330ac2b2c286d723e1784421a15e723e8cefe0
Francisco Souza <francisco@franciscosouza.net>
2010-07-30 17:02:39 -0300

    Registering the dateformat filter

--- talks_application/__init__.py
+++ talks_application/__init__.py
@@ -5,3 +5,4 @@ app = Flask('talks')
 app.config.from_object('talks_application.settings')
 
 import actions
+import filters
--- talks_application/filters.py
+++ talks_application/filters.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 """
-Module containing templates filter for using in Jinja
+Module containing template filters for using in Jinja
 
 Author: Francisco Souza
 """
+from talks_application import app
 
+@app.template_filter('dateformat')
 def dateformat(value, fmt = '%m/%d/%Y'):
     """Formats a date object (datetime.date instance) to
     the specified output format"""
