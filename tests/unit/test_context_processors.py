76c383fc860f4200aa3864bf42cf3dc1966c1962
Francisco Souza <francisco@franciscosouza.net>
2010-07-30 18:12:36 -0300

    Also injecting logout URL on the template

--- tests/unit/test_context_processors.py
+++ tests/unit/test_context_processors.py
@@ -3,8 +3,9 @@ from nose.tools import assert_equals
 
 class TestContextProcessors(unittest.TestCase):
 
-    def test_create_login_url_injector(self):
-        from google.appengine.api.users import create_login_url
+    def test_create_login_and_logout_url_injector(self):
+        "Should exists a context processor to inject the Google App Engine login and logout URLs in the context"
+        from google.appengine.api.users import create_login_url, create_logout_url
         from talks_application.context_processors import inject_login_url
-        assert_equals(inject_login_url(), dict(create_login_url=create_login_url))
+        assert_equals(inject_login_url(), dict(create_login_url=create_login_url, create_logout_url=create_logout_url))
 
