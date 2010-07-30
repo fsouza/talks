6e88f4d182240d5c2ae7304a995fced28b414791
Francisco Souza <francisco@franciscosouza.net>
2010-07-30 16:58:38 -0300

    Touch 10  of rating in Pylint

--- talks_application/filters.py
+++ talks_application/filters.py
@@ -1,4 +1,11 @@
-#coding:utf-8
+# coding: utf-8
+"""
+Module containing templates filter for using in Jinja
 
-def dateformat(value, format = '%m/%d/%Y'):
-    return value.strftime(format)
+Author: Francisco Souza
+"""
+
+def dateformat(value, fmt = '%m/%d/%Y'):
+    """Formats a date object (datetime.date instance) to
+    the specified output format"""
+    return value.strftime(fmt)
